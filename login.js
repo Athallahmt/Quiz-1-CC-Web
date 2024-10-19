05a38afbe1151882ccc3d25b2da01102d0446e73
Athallahmt <148701765+Athallahmt@users.noreply.github.com>
2024-10-19 18:58:18 +0700

    Update login.js

--- login.js
+++ login.js
@@ -8,9 +8,10 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
     // Simulasi login: admin dan user
     if (email === 'admin@beyonce.com' && password === 'beyonce123') {
         window.location.href = 'about.html'; // Redirect ke halaman about untuk admin
-    } else if (email === 'user@ejayz.com' && password === 'jayz123') {
+    } else if (email === 'user@jayz.com' && password === 'jayz123') {
         window.location.href = 'home.html'; // Redirect ke halaman home untuk user
     } else {
         errorMessage.innerText = 'Incorrect Password! Please try again';
     }
 });
+
